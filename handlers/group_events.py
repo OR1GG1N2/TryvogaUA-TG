0733f20980c273552039b96695a2089808432ffb
Or1GG1n <json57283@gmail.com>
2025-01-16 17:20:00 +0200

    Send alarm map

--- handlers/group_events.py
+++ handlers/group_events.py
@@ -1,26 +1,54 @@
+from io import BytesIO
+
+import requests
 import structlog
 from aiogram import Router, F, Bot
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command, CommandStart, CommandObject
-from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, InlineKeyboardMarkup
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, InlineKeyboardMarkup, BufferedInputFile
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-
+from PIL import Image
 from fluent.runtime import FluentLocalization
 
 # Declare router
 router = Router()
-router.message.filter(F.chat.type == "group") # process group events only
+router.message.filter(F.chat.type.in_(['group', 'supergroup'])) # process group events only
 
 # Declare logger
 logger = structlog.get_logger()
 
-# Declare handlers
-@router.message(F.content_type.in_({'new_chat_members', 'left_chat_member'}))
-async def on_user_join_or_left(message: Message):
-    """
-    Removes "user joined" and "user left" messages.
-    By the way, bots do not receive left_chat_member updates when the group has more than 50 members (otherwise use https://core.telegram.org/bots/api#chatmemberupdated)
-    :param message: Service message "User joined group
-    """
+#
+IMAGE_URL = "https://alert-node.vercel.app/api/map"
 
-    await message.delete()
+# Declare handlers
+# @router.message(F.content_type.in_({'new_chat_members', 'left_chat_member'}))
+# async def on_user_join_or_left(message: Message):
+#     """
+#     Removes "user joined" and "user left" messages.
+#     By the way, bots do not receive left_chat_member updates when the group has more than 50 members (otherwise use https://core.telegram.org/bots/api#chatmemberupdated)
+#     :param message: Service message "User joined group
+#     """
+#
+#     await message.delete()
+
+@router.message(F.text.lower().in_(['тривога', 'тревога']))
+async def send_image_on_trigger(message: Message):
+    response = requests.get(IMAGE_URL)
+    response.raise_for_status()  # Проверяем, что запрос успешен
+
+    with BytesIO(response.content) as image_stream:
+        im = Image.open(image_stream).convert("RGBA")  # Оставляем прозрачность
+
+        # Сохраняем в BytesIO
+        output_stream = BytesIO()
+        im.save(output_stream, format="webp", lossless=True)  # Поддержка прозрачности и без потерь
+        output_stream.seek(0)  # Сброс указателя
+
+        # Создаем BufferedInputFile
+        sticker_file = BufferedInputFile(output_stream.getvalue(), filename="sticker.webp")
+
+    # Отправляем как стикер
+    await message.answer_sticker(sticker=sticker_file)
+
+
+
--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -2,10 +2,14 @@ import structlog
 from aiogram import Router, F, Bot
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command, CommandStart, CommandObject
-from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, InlineKeyboardMarkup
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, InlineKeyboardMarkup, InputFile, BufferedInputFile
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-
+import requests
 from fluent.runtime import FluentLocalization
+
+import io
+from io import BytesIO
+from PIL import Image
 
 # Declare router
 router = Router()
@@ -13,6 +17,8 @@ router.message.filter(F.chat.type == "private")
 
 # Declare logger
 logger = structlog.get_logger()
+IMAGE_URL = "https://alert-node.vercel.app/api/map"
+
 
 # Declare handlers
 @router.message(Command("start"))
@@ -24,3 +30,24 @@ async def cmd_owner_hello(message: Message, l10n: FluentLocalization):
 @router.message(F.content_type.in_({'photo', 'video'}))
 async def cmd_media_react_bot(message: Message, l10n: FluentLocalization):
     await message.reply(l10n.format_value("media-msg"))
+
+
+
+@router.message(F.text.lower().in_(['тривога', 'тревога']))
+async def send_image_on_trigger(message: Message):
+    response = requests.get(IMAGE_URL)
+    response.raise_for_status()  # Проверяем, что запрос успешен
+
+    with BytesIO(response.content) as image_stream:
+        im = Image.open(image_stream).convert("RGBA")  # Оставляем прозрачность
+
+        # Сохраняем в BytesIO
+        output_stream = BytesIO()
+        im.save(output_stream, format="webp", lossless=True)  # Поддержка прозрачности и без потерь
+        output_stream.seek(0)  # Сброс указателя
+
+        # Создаем BufferedInputFile
+        sticker_file = BufferedInputFile(output_stream.getvalue(), filename="sticker.webp")
+
+    # Отправляем как стикер
+    await message.answer_sticker(sticker=sticker_file)
